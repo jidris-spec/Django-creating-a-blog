f028af214292a1cd1e53522a70560d9661cfe85b
Idris <jidris64@gmail.com>
2024-07-23 11:27:37 +0200

    new update

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import PostModel
 from .forms import PostModelForm
+# import  get_object_or_404
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 def index(request):
@@ -9,11 +13,14 @@ def index(request):
         form = PostModelForm(request.POST)
         if form.is_valid():
             instance = form.save(commit=False)
+            user = get_object_or_404(User, username=request.user)
             instance.author = request.user
             instance.save()
             return redirect('index')
     else:
         form = PostModelForm()
+        print(request.user)
+
     context = {
         'posts': posts,
         'form': form,
